c43dac7cf652eadeb0def7b871aba526875877dc
itsmohamedyahia <justmyahia@gmail.com>
2022-12-31 01:47:21 +0200

    arrange repo

---
+++ 2-programming-with-js/wk2/wk2-exercise-CreatingArraysAndObjects.js
@@ -0,0 +1,9 @@
+var clothes = [];
+clothes.push("Shirt", "Shorts", "Socks", "Jacket", "T-shirt");
+clothes.pop();
+clothes.push("Apex Legends T-shirt");
+console.log(clothes[2]);
+var favCar = {};
+favCar.color = "Red";
+favCar.covertible = true;
+console.log(favCar);
